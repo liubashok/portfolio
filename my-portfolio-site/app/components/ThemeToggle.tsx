f68d47a0a41e92c91c73f5b756a83f8a3644dcd1
liubashok <liubashok@gmail.com>
2025-07-06 19:59:26 +0200

    fix mistakes

--- my-portfolio-site/app/components/ThemeToggle.tsx
+++ my-portfolio-site/app/components/ThemeToggle.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export function ThemeToggle() {
   const { theme, setTheme } = useTheme();
