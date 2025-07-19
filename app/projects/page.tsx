b990e32ba664ddfd1cf00f06273812b6ea9fd0a7
liubashok <liubashok@gmail.com>
2025-07-20 01:21:48 +0200

    change projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -7,22 +7,22 @@ type Project = {
 
 const projects: Project[] = [
   {
-    title: "Сайт йога-студии",
-    description: "Лендинг со встроенной формой бронирования",
-    image: "#",
-    link: "#",
+    title: "Лендинг яхт-клуба",
+    description: "Лендинг, выполненный в рамках обучения вёрстке",
+    image: "landing-logo.svg",
+    link: "https://liubashok.github.io/my-landing-project/",
   },
   {
-    title: "Интернет-магазин товаров для животных",
-    description: "Полностью адаптивный магазин с корзиной",
-    image: "#",
-    link: "#",
+    title: "Интернет-магазин косметики",
+    description: "Тестовый проект для изучения React и Redux",
+    image: "makeup.png",
+    link: "https://liubashok.github.io/makeup_products/",
   },
 ];
 
 export default function ProjectsPage() {
   return (
-    <section className="absolute left-1/2 -translate-x-1/2 ">
+    <section className="absolute min-w-[350px] left-1/2 -translate-x-1/2 z-20">
       <h1 className="text-3xl font-bold mb-6">Проекты</h1>
       <div className="flex flex-col sm:flex-row gap-4">
         {projects.map((project) => (
@@ -31,15 +31,17 @@ export default function ProjectsPage() {
             href={project.link}
             target="_blank"
             rel="noopener noreferrer"
-            className="border z-20 rounded-lg p-4 hover:shadow-lg transition"
+            className="border flex z-20 rounded-lg gap-4 p-4 hover:shadow-lg transition"
           >
             <img
               src={project.image}
               alt={project.title}
               className="rounded mb-4"
             />
-            <h2 className="text-xl font-semibold">{project.title}</h2>
-            <p className="text-sm text-gray-600">{project.description}</p>
+            <div>
+              <h2 className="text-xl font-semibold">{project.title}</h2>
+              <p className="text-sm text-gray-600">{project.description}</p>
+            </div>
           </a>
         ))}
       </div>
