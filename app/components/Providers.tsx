25eb3bdd5bc1649afbbe1e417d22aba70da88ea8
liubashok <liubashok@gmail.com>
2025-07-31 00:55:41 +0200

    fix theme bug

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -9,6 +9,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
       defaultTheme="system"
       enableSystem
       disableTransitionOnChange
+      enableColorScheme={false}
     >
       {children}
     </ThemeProvider>
