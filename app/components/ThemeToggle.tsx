bb9a8cb5beb7813b60b3fc86f004753e6cb53e85
liubashok <liubashok@gmail.com>
2025-07-30 20:45:14 +0200

    fix ThemeToggle

--- app/components/ThemeToggle.tsx
+++ app/components/ThemeToggle.tsx
@@ -5,31 +5,33 @@ import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 
 export function ThemeToggle() {
-  const { theme, setTheme } = useTheme();
+  const { resolvedTheme, setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
 
-  useEffect(() => setMounted(true), []);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
   if (!mounted) return null;
+
+  const isDark = resolvedTheme === "dark";
 
   return (
     <button
       style={
-        theme === "dark"
+        isDark
           ? { backgroundImage: "url('/moon.png')" }
-          : {
-              backgroundColor: "white",
-            }
+          : { backgroundColor: "white" }
       }
-      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      onClick={() => setTheme(isDark ? "light" : "dark")}
       className={cn(
         "z-10 sm:mt-10 mt-4 mr-4 sm:mr-10 relative h-20 w-20 rounded-full",
-        theme === "dark"
+        isDark
           ? "hover:shadow-[0_0_10px_10px_rgba(255,255,255,0.3)]"
           : "sun-glow shadow-[inset_0_0_15px_rgba(255,255,0,1)] hover:shadow-[0_0_80px_10px_rgba(255,255,255,0.1)]"
       )}
     >
-      {theme === "dark" ? (
+      {isDark ? (
         <div className="absolute top-0 -translate-y-[10%] shadow-[0_0_10px_10px_rgba(0,0,0,1)] bg-black rounded-full h-16 z-10 w-16 translate-x-[40%]" />
       ) : (
         <div />
